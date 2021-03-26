ae4d76dfa630e178b92cb4efa82427501e418aaf
FlutterRaj <rajkar921@gmail.com>
2021-03-27 00:19:56 +0530

    static files added

---
+++ static/js/password.js
@@ -0,0 +1,10 @@
+// For show and hide password
+$(".toggle-password").click(function () {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
